7290480132b4ad7740987ada2d1f07a917e391ef
Zi_su <aitona765@gmail.com>
2023-09-19 00:43:08 +0900

    add

--- Source/KotoriUI/Private/KotoriUI.cpp
+++ Source/KotoriUI/Private/KotoriUI.cpp
@@ -1,20 +1,20 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "KotoriGameUI.h"
+#include "../Public/KotoriUI.h"
 
-#define LOCTEXT_NAMESPACE "FKotoriGameUIModule"
+#define LOCTEXT_NAMESPACE "FKotoriUIModule"
 
-void FKotoriGameUIModule::StartupModule()
+void FKotoriUIModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 }
 
-void FKotoriGameUIModule::ShutdownModule()
+void FKotoriUIModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
-	
-IMPLEMENT_MODULE(FKotoriGameUIModule, KotoriGameUI)
+
+IMPLEMENT_MODULE(FKotoriUIModule, KotoriUI)
--- Source/KotoriUI/Public/KotoriUI.h
+++ Source/KotoriUI/Public/KotoriUI.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-class FKotoriGameUIModule : public IModuleInterface
+class FKotoriUIModule : public IModuleInterface
 {
 public:
 
